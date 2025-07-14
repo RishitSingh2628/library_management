67a87da5148591823921d24d456d4ed5bf6e20b2
RishitSingh2628 <rishit261203@gmail.com>
2025-07-14 19:13:27 +0530

    Update urls.py

--- library_management/urls.py
+++ library_management/urls.py
@@ -17,6 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view as get_yasg_schema_view
+from drf_yasg import openapi
+
+schema_view=get_yasg_schema_view(
+    openapi.Info(
+        title="Library Management",
+        default_version='v1',
+        description='API documentation'
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,6 +38,8 @@ urlpatterns = [
         description="API for all endpoints",
         version="1.0.0"
     ), name='openapi-schema'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swaggerUI'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
     path('api/', include('books.urls')),
     path('api/', include('members.urls')),
     path('api/', include ('transactions.urls')),
